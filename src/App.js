07ab294aee7c699bf6cd86865fdaca6ec1be845e
Renato Ferreira <renatoferreira@MacBook-Pro.domain_not_set.invalid>
2020-06-18 15:26:05 +0200

    initial setup

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.scss";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Initial setup</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
