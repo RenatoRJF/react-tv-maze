a82d39cffbd8abe2e344592fd941060f427b8209
Renato Ferreira <renatoferreira@MacBook-Pro.domain_not_set.invalid>
2020-06-19 19:19:07 +0200

    Customize EpisodeDetails component

--- src/components/EpisodeDetails/index.js
+++ src/components/EpisodeDetails/index.js
@@ -1,5 +1,61 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { connect } from "react-redux";
+import SanitizedHTML from "react-sanitized-html";
 
-export default function EpisodeDetails() {
-  return <h1>Show details</h1>;
+import { getEpisodeByNumber } from "../../store/actions/shows";
+import Layout from "../Layout";
+import DefaultImage from "../../images/default.jpg";
+import "./episode-details.scss";
+
+export function EpisodeDetails({ getEpisode, episode }) {
+  const { showId, seasonNumber, episodeNumber } = useParams();
+  const { image, name, summary } = episode || {};
+
+  useEffect(() => {
+    getEpisode(showId, seasonNumber, episodeNumber);
+  }, [episodeNumber, getEpisode, seasonNumber, showId]);
+
+  return (
+    <Layout>
+      <div className="episode__details">
+        {episode && (
+          <>
+            <div className="episode__image">
+              {!image ? (
+                <img src={DefaultImage} alt="episode" />
+              ) : (
+                <img src={image?.original} alt="episode" />
+              )}
+            </div>
+
+            <h1 className="episode__title">{name}</h1>
+
+            <SanitizedHTML
+              className="episode__description"
+              allowedAttributes={{ a: ["href"] }}
+              allowedTags={["a", "p", "em", "i", "strong", "span"]}
+              html={summary || "No description"}
+            />
+          </>
+        )}
+      </div>
+    </Layout>
+  );
 }
+
+const mapStateToProps = (state) => {
+  return {
+    episode: state.currentEpisode,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    getEpisode: (showId, seasonNumber, episodeNumber) => {
+      return dispatch(getEpisodeByNumber(showId, seasonNumber, episodeNumber));
+    },
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(EpisodeDetails);
