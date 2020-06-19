799647cfd18be2ac5d9adb8d406105280ad8e964
Renato Ferreira <renatoferreira@MacBook-Pro.domain_not_set.invalid>
2020-06-19 19:16:30 +0200

    Customize Active episode

--- src/components/EpisodesList/index.js
+++ src/components/EpisodesList/index.js
@@ -1,19 +1,39 @@
 import React from "react";
 import "./episodes-list.scss";
+import { Link, useParams } from "react-router-dom";
+import DefaultImage from "../../images/default.jpg";
 
 export default function EpisodesList({ episodes }) {
+  const { showId, episodeNumber, seasonNumber } = useParams();
+
   return (
     <div className="episodes__list">
       {Array.isArray(episodes) &&
-        episodes.map(({ id, image, name, number }) => (
-          <div className="episode" key={id}>
-            <div className="image">
-              <img src={image?.medium} alt="episode" />
-            </div>
+        episodes.map(({ id, image, name, number, season }) => {
+          let isActive = number === 1;
 
-            <h4>{`${number} - ${name}`}</h4>
-          </div>
-        ))}
+          if (episodeNumber && Number(seasonNumber) === season) {
+            isActive = Number(episodeNumber) === number;
+          }
+
+          return (
+            <Link
+              className={`episode ${isActive ? "active" : ""}`}
+              key={id}
+              to={`/show/${showId}/season/${season}/episode/${number}`}
+            >
+              <div className="image">
+                {image ? (
+                  <img src={image?.medium} alt="episode" />
+                ) : (
+                  <img src={DefaultImage} alt="episode" />
+                )}
+              </div>
+
+              <h4>{`${number} - ${name}`}</h4>
+            </Link>
+          );
+        })}
     </div>
   );
 }
