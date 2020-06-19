4b19837abb8fc42761d1a4d3d5526f6ebd02c6e9
Renato Ferreira <renatoferreira@MacBook-Pro.domain_not_set.invalid>
2020-06-19 11:11:05 +0200

    Customize ShowDetails component

--- src/components/ShowDetails/index.js
+++ src/components/ShowDetails/index.js
@@ -1,5 +1,63 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { connect } from "react-redux";
+import SanitizedHTML from "react-sanitized-html";
 
-export default function ShowDetails() {
-  return <h1>Show details</h1>;
+import { getCurrentShow } from "../../store/actions/shows";
+import Layout from "../Layout";
+import "./show-details.scss";
+
+export function ShowDetails({ getShow, show }) {
+  const { id } = useParams();
+  const { image, name, summary, type, genres } = show || {};
+
+  useEffect(() => {
+    getShow(id);
+  }, [id, getShow]);
+
+  return (
+    <Layout>
+      <div className="show__details">
+        {show && (
+          <>
+            <div className="show__image">
+              <img src={image?.original} alt="show" />
+            </div>
+
+            <h1 className="show__title">{name}</h1>
+
+            <SanitizedHTML
+              className="show__description"
+              allowedAttributes={{ a: ["href"] }}
+              allowedTags={["a", "p", "em", "i", "strong", "span"]}
+              html={summary}
+            />
+
+            <span className="show__type">
+              <strong>Type:</strong> {type}
+            </span>
+
+            <span className="show__genres">
+              <strong>Genres:</strong>
+              {genres && genres.join(", ")}
+            </span>
+          </>
+        )}
+      </div>
+    </Layout>
+  );
 }
+
+const mapStateToProps = (state) => {
+  return {
+    show: state.currentShow,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    getShow: (id) => dispatch(getCurrentShow(id)),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(ShowDetails);
