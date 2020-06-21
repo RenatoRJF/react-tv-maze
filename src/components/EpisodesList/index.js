3ca13837a85045b61fc2a6f0b0c392754b16cc41
Renato Ferreira <renatoferreira@MacBook-Pro.domain_not_set.invalid>
2020-06-21 12:30:16 +0200

    Add prop-types to EpisodesList component

--- src/components/EpisodesList/index.js
+++ src/components/EpisodesList/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 import "./episodes-list.scss";
 import { Link, useParams } from "react-router-dom";
 import DefaultImage from "../../images/default.jpg";
@@ -42,3 +43,19 @@ export default function EpisodesList({ episodes, isLoading }) {
     </div>
   );
 }
+
+EpisodesList.propTypes = {
+  episodes: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      image: PropTypes.shape({
+        medium: PropTypes.string,
+        original: PropTypes.string,
+      }),
+      name: PropTypes.string.isRequired,
+      season: PropTypes.number.isRequired,
+      number: PropTypes.number,
+    })
+  ),
+  isLoading: PropTypes.bool.isRequired,
+};
